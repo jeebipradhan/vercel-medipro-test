429ac5083661bd42db2d00fdc2dd3573b32fec7c
Gyanendra Pradhan <jeebipradhan@gmail.com>
2024-11-13 20:15:04 -0700

    minor update

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
       <footer className="mt-16 border-t border-white/10 py-8">
         <div className="max-w-7xl mx-auto px-6 text-center">
-          <p>&copy; 2024 MEDIPRO. All rights reserved.</p>
+          <p>&copy; 2024 MediPro. All rights reserved.</p>
         </div>
       </footer>
     )
